d65183a9781b3db0f02e7204ab9d13a481759036
Sharath <sharathnair9999@gmail.com>
2022-01-12 15:32:43 +0530

    1st easy problem

---
+++ easy1.js
@@ -0,0 +1,8 @@
+// Write a program to add 5 numbers. The value of numbers are num1=5, num2=13, num3=7, num4=21 and num5=48.
+
+var num1 = 5
+var num2 = 13
+var num3 = 7
+var num4 = 21
+var num5=48
+console.log(num1+num2+num3+num4+num5);
