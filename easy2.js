39c762687d6372edd801b24b24a5f23285116848
Sharath <sharathnair9999@gmail.com>
2022-01-12 15:40:18 +0530

    easy2

---
+++ easy2.js
@@ -0,0 +1,7 @@
+// 2. Write a program to take a number input from user and determine whether the number is odd or even.
+
+const num = 11;
+
+const isEven = number => number%2===0
+
+console.log(isEven(num));
